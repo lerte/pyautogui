ae4c89961c761e4310bc2aa1235a73fb1462d286
lerte smith <lerte@qq.com>
2023-03-02 22:03:32 +0800

    使用winsound

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import os
 import time
+import winsound
 import pyautogui
-from playsound import playsound
-
-alarm = os.path.dirname(__file__)+'/alarm.mp3'
-print(alarm)
 
 while True:
   location = pyautogui.locateOnScreen('ore.png', confidence=0.9)
   if location:
     print('发现矿石{}',location)
-    pyautogui.alert(text='发现矿石', title='消息提示')
-    playsound(alarm)
+    # pyautogui.alert(text='发现矿石', title='消息提示')
+    winsound.PlaySound('alarm.wav', winsound.SND_FILENAME)
     time.sleep(5)
   else:
     print('没有找到，继续寻找中...')
